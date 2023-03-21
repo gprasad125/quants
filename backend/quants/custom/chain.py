50e41548c34df6bb00d996c78fc6a8821c10156e
proxim <61657192+proxim@users.noreply.github.com>
2023-03-21 00:31:52 -0500

    refactor chain.py
    - added more documentation
    - double to single quotes
    - changed keys from strings to constants

--- backend/quants/custom/chain.py
+++ backend/quants/custom/chain.py
@@ -1,9 +1,7 @@
 from langchain.chains import VectorDBQAWithSourcesChain
-from langchain.chains.combine_documents.map_reduce import MapReduceDocumentsChain
-from typing import Any, Dict, List, Tuple
-
-from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain.chains.llm import LLMChain
+from langchain.chains.combine_documents.map_reduce import MapReduceDocumentsChain
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain.chains.qa_with_sources.map_reduce_prompt import (
     COMBINE_PROMPT,
     EXAMPLE_PROMPT,
@@ -12,10 +10,16 @@ from langchain.chains.qa_with_sources.map_reduce_prompt import (
 from langchain.docstore.document import Document
 from langchain.llms.base import BaseLLM
 from langchain.prompts.base import BasePromptTemplate
+from typing import Any, Dict, List, Tuple
 
 class CustomMapReduceChain(MapReduceDocumentsChain):
-        """Combining documents by mapping a chain over them, then combining results."""
+        """
+        Combining documents by mapping a chain over them, then combining results.
         
+        Edited from LangChain's MapReduceDocumentsChain:
+        https://github.com/hwchase17/langchain/blob/master/langchain/chains/combine_documents/map_reduce.py 
+        """
+
         def _process_results(
             self,
             results: List[Dict],
@@ -23,8 +27,6 @@ class CustomMapReduceChain(MapReduceDocumentsChain):
             token_max: int = 3000,
             **kwargs: Any,
         ) -> Tuple[str, dict]:
-            
-            # overriding _process_results() in MapReduceDocumentsChain in main method to return result_docs
             question_result_key = self.llm_chain.output_key
             result_docs = [
                 Document(page_content=r[question_result_key], metadata=docs[i].metadata)
@@ -48,14 +50,21 @@ class CustomMapReduceChain(MapReduceDocumentsChain):
                 )
             if self.return_intermediate_steps:
                 _results = [r[self.llm_chain.output_key] for r in results]
-                extra_return_dict = {"intermediate_steps": _results}
+                extra_return_dict = {'intermediate_steps': _results}
             else:
                 extra_return_dict = {}
             output, _ = self.combine_document_chain.combine_docs(result_docs, **kwargs)
-
+            # overriding _process_results() to return result_docs
+            # so that we can extract the relevant snippets to highlight for each source
             return output, extra_return_dict, result_docs
         
 class CustomChain(VectorDBQAWithSourcesChain):
+    """
+    Custom chain built from LangChain's VectorDBQAWithSourcesChain.
+    """
+    document_key = 'docs'
+    snippets_key = 'snippets'
+
     @classmethod
     def from_llm(
         cls,
@@ -71,12 +80,13 @@ class CustomChain(VectorDBQAWithSourcesChain):
         combine_results_chain = StuffDocumentsChain(
             llm_chain=llm_combine_chain,
             document_prompt=document_prompt,
-            document_variable_name="summaries",
+            document_variable_name='summaries',
         )
+        # override this method to use our own CustomMapReduceChain
         combine_document_chain = CustomMapReduceChain(
             llm_chain=llm_question_chain,
             combine_document_chain=combine_results_chain,
-            document_variable_name="context",
+            document_variable_name='context',
         )
         return cls(
             combine_documents_chain=combine_document_chain,
@@ -85,19 +95,39 @@ class CustomChain(VectorDBQAWithSourcesChain):
 
     @property
     def output_keys(self) -> List[str]:
-        """Return output key.
-        :meta private:
         """
-        return [self.answer_key, self.sources_answer_key, 'docs', 'result_docs']
+        Returns a list of valid output keys.
+        """
+        # override this method to include document_key and snippets_key
+        return [self.answer_key, self.sources_answer_key, self.document_key, self.snippets_key]
     
     def _call(self, inputs: Dict[str, Any]) -> Dict[str, str]:
+        """
+        Run the query through the custom chain.
+
+        Args:
+            inputs: a dict containing a string key and value, used for asking the question
+        Returns:
+            a dict containing the following key-value pairs:
+                'answer': the result generated from the LLM
+                'sources': a string of the UUIDs of the source documents separated by commas
+                'docs': a list of LangChain Documents containing the full content of each source
+                    document used for answering the question in the page_contents field
+                'snippets': a list of LangChain Documents containing the extracts from source
+                    documents used for answering the question in the page_contents field
+        """
         docs = self._get_docs(inputs)
-        # print(docs[0])
-        answer, _, result_docs = self.combine_documents_chain.combine_docs(docs, **inputs)
-        if "SOURCES:" in answer:
-            answer, sources = answer.split("SOURCES:")
+        answer, _, snippets = self.combine_documents_chain.combine_docs(docs, **inputs)
+        if 'SOURCES:' in answer:
+            answer, sources = answer.split('SOURCES:')
         else:
-            sources = ""
-        return {self.answer_key: answer, self.sources_answer_key: sources, 'docs': docs, 'result_docs': result_docs}
+            sources = ''
+
+        return {
+            self.answer_key: answer, 
+            self.sources_answer_key: sources, 
+            self.document_key: docs, 
+            self.snippets_key: snippets
+        }
     
 
